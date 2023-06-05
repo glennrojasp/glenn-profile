0b7bb6b43376dc44d900aca3ef9a528895cd15ec
glennrojasp <glenn.rojasp@gmail.com>
2023-06-05 11:32:38 -0600

    Updating hero with new profile links

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 
 import './hero.style.css'
 
@@ -8,7 +11,12 @@ export const Hero = props =>(
         <div className="intro-text">
           <div className="intro-lead-in">{props.subTitle}</div>
           <div className="intro-heading">{props.title}</div>
-          <div className="intro-heading text-uppercase"></div>
+          <div className="quick-links">
+            <ul>
+              <li><a href="https://www.linkedin.com/in/glenn-rojas-pineda-99287531/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="fa fa-facebook"/></a></li>
+              <li><a href="https://github.com/glennrojasp" target="_blank"><FontAwesomeIcon icon={faGithub} className="fa fa-facebook"/></a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
